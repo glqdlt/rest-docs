884599c5cfc2586d8c8487bb3fa1027a4a378383
glqdlt <dlfdnd0725@gmail.com>
2019-04-25 17:32:51 +0900

    spring-rest-docs 생성

--- src/main/java/com/glqdlt/ex/springrestdocs/CounterRestController.java
+++ src/main/java/com/glqdlt/ex/springrestdocs/CounterRestController.java
@@ -18,6 +18,12 @@ public class CounterRestController {
 
     private CounterService greetingService;
 
+    /**
+     * @see <a href='https://www.baeldung.com/spring-rest-docs'>https://www.baeldung.com/spring-rest-docs</a>
+     * @ses <a href='https://scacap.github.io/spring-auto-restdocs/#snippets-path-parameters'>https://scacap.github.io/spring-auto-restdocs/#snippets-path-parameters</a>
+     * @param user target user name
+     * @return
+     */
     @GetMapping("/api/{user}/greeting")
     public String greeting(@PathVariable(name = "user") String user) {
         return greetingService.greeting(user);
